005adf545092ef8a83d7d5acd0d1e10e3ef4b9e0
TCO46 <patohru2006@gmail.com>
2025-12-15 14:26:38 +0700

    ci: fix format

--- assets.go
+++ assets.go
@@ -21,16 +21,16 @@ func getAssetPath(videoID uuid.UUID, mediaType string) string {
 	return fmt.Sprintf("%s%s", videoID, ext)
 }
 
-func(cfg apiConfig) getAssetDiskPath(path string) string {
+func (cfg apiConfig) getAssetDiskPath(path string) string {
 	return filepath.Join(cfg.assetsRoot, path)
 }
 
-func(cfg apiConfig) getAssetURL(assetPath string) string {
+func (cfg apiConfig) getAssetURL(assetPath string) string {
 	return fmt.Sprintf("http://localhost:%s/assets/%s", cfg.port, assetPath)
 }
 
 func mediaTypeToExt(mediaType string) string {
-	splitStr := strings.Split(mediaType, "/")	
+	splitStr := strings.Split(mediaType, "/")
 	if len(splitStr) < 2 {
 		return ".bin"
 	}
